0a5219e4202bc97062f70a33e60e1285f1891abb
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-07-05 15:04:01 +0530

    Update timezone.sh

--- timezone.sh
+++ timezone.sh
@@ -3,7 +3,7 @@ echo "Current TimeZone is"
 timedatectl
 echo ""
 echo "Changing Timezone to IST"
-
+-----------------------------------------
 sudo timedatectl set-timezone "Asia/Kolkata"
 echo "TimeZone sucesfully changed to IST"
-
+timedatectl
